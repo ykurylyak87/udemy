62b81ca5b95590a4e62c36914957a506c7da2358
ykuryl <ykurylyak@gmail.com>
2018-03-19 13:48:23 +0200

    lists

--- basicsyntax/tuplesdemo.py
+++ basicsyntax/tuplesdemo.py
@@ -3,3 +3,24 @@ Tuple
 Like list but they are immutable
 It means you can't change them
 """
+
+my_list = [1, 2, 3]
+print(my_list)
+
+print("*"*20)
+my_list[0] = 0
+print(my_list)
+
+print("*"*20)
+my_tuple = (1, 2, 3)
+# my_tuple[0] = 0 -> you can't do like this! Tuple doesn't support item assignment
+print(my_tuple)
+print(my_tuple[0])
+print(my_tuple[1:])
+
+print("*"*20)
+print("Index of 2 in tuple -> " + str(my_tuple.index(2)))
+
+print("*"*20)
+my_tuple = (1, 2, 3, 4, 5, 6, 7, 1, 1)
+print("Count how many '1' are in tuple -> " + str(my_tuple.count(1)))
