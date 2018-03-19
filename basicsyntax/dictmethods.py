fea8869586808b387ec865fe11ee47b54c3ab0a2
ykuryl <ykurylyak@gmail.com>
2018-03-19 13:23:16 +0200

    lists

---
+++ basicsyntax/dictmethods.py
@@ -0,0 +1,30 @@
+"""
+Dictionary Methods
+"""
+car = {'make': 'bmw', 'model': '550i', "year": 2016}
+cars = {'bmw': {'model': '550i', 'year': 2016}, 'benz': {'model': 'E350', 'year': 2015}}
+
+print(car.keys())
+print(cars.keys())
+
+print("*"*20)
+print(car.values())
+print(cars.values())
+
+print("*"*20)
+print(car.items())
+print(cars.items())
+
+print("*"*20)
+car_copy = car.copy()
+print(car_copy)
+
+print("*"*20)
+print(car.pop('model'))
+print(car)
+
+print("*"*20)
+car.clear()
+print(car)
+
+print("*"*20)
