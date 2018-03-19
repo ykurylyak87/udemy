8fad967ae0af4dfbaec70b983ee1d43dab2c4635
ykuryl <ykurylyak@gmail.com>
2018-03-19 10:49:08 +0200

    lists

--- basicsyntax/listsmethods.py
+++ basicsyntax/listsmethods.py
@@ -1,3 +1,23 @@
 cars = ["bmw", "honda", "audi"]
 
-len(cars)
+length = len(cars)
+print(length)
+
+cars.append("benz")
+print(cars)
+
+cars.insert(1, "jeep")
+print(cars)
+
+x = cars.index("honda")
+print(x)
+
+y = cars.pop()
+print(y)
+print(cars)
+
+cars.remove("jeep")
+print(cars)
+
+slicing = cars[0:2]
+print(slicing)
