ac97220a11bc85fee976013dd3c180929ae03e08
ykuryl <ykurylyak@gmail.com>
2018-03-21 14:29:00 +0200

    methods

---
+++ methods/methodexercise.py
@@ -0,0 +1,17 @@
+l = {'nyc': 0.20, 'la': 0.22, 'nc': 0.15}
+fed_tax = 0.10
+
+
+def state_tax(state):
+    return l[state]
+
+
+def net_tax(gross):
+    state_name = input("Input the name of state: ")
+    net_value = gross - (gross * state_tax(state_name)) - (gross * fed_tax)
+    return net_value
+
+
+x = float(input("Input gross value: "))
+y = net_tax(x)
+print("Your net value is: " + str(y))
