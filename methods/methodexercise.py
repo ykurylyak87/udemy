fc7509cd0ad9ffe7a33d1ec2c2c0dd1a27ac2735
ykuryl <ykurylyak@gmail.com>
2018-03-22 10:11:30 +0200

    net price

--- methods/methodexercise.py
+++ methods/methodexercise.py
@@ -2,19 +2,14 @@ l = {'nyc': 0.20, 'la': 0.22, 'nc': 0.15}
 fed_tax = 0.10
 
 
-def state_tax(state):
-    return l[state]
-
-
 def net_tax(gross):
     state_name = input("Input the name of state: ")
     if state_name in l:
-        net_value = gross - (gross * state_tax(state_name)) - (gross * fed_tax)
+        net_value = gross - (gross * l[state_name]) - (gross * fed_tax)
+        print("Your net value is: " + str(net_value))
         return net_value
     else:
+        print("The input state is not in base")
         return None
 
-
-x = float(input("Input gross value: "))
-y = net_tax(x)
-print("Your net value is: " + str(y))
+net_tax(float(input("Input gross value: ")))
