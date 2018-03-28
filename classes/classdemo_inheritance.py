0d42accb4fb960bbcd6932a659dd72d56ef8aec8
ykuryl <ykurylyak@gmail.com>
2018-03-28 18:36:54 +0300

    classes

---
+++ classes/classdemo_inheritance.py
@@ -0,0 +1,23 @@
+class Car(object):
+
+    def __init__(self):
+        print("You just created the car instance")
+
+    def drive(self):
+        print("Car started ...")
+
+    def stop(self):
+        print("Car stopped")
+
+class BMW(Car):
+
+    def __init__(self):
+        print("You just create the BMW instance")
+
+c = Car()
+c.drive()
+c.stop()
+
+b = BMW()
+b.stop()
+b.drive()
