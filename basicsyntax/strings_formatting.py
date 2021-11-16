65443efe9c39a67abce345a507a5978657bbfc90
ykurylyak87 <ykurylyak@gmail.com>
2021-11-16 18:13:12 +0200

    formatting

--- basicsyntax/strings_formatting.py
+++ basicsyntax/strings_formatting.py
@@ -4,3 +4,4 @@ event = "show"
 print("Welcome to " + city + " and enjoy the " + event)
 print("Welcome to %s" % city)
 print("Welcome to %s and enjoy the %s" % (city, event))
+print(f"Welcome to {city} and enjoy the {event}")
